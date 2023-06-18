4d2c634d98790202b2b2037323bddacbc63cd71b
Hamza Farooq <131591719+HamzahFarooqq@users.noreply.github.com>
2023-06-18 23:08:37 +0500

    for loop in Js.

---
+++ task-21/script.js
@@ -0,0 +1,22 @@
+// Use of loop to display a string of all the names of months
+// using JS functions and variables.
+
+const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
+
+for (let i = 0; i < months.length; i++) {
+  let allmonths = months[i];
+  console.log(allmonths);
+}
