a13848823388e6fbefc8803901d29373fb098850
Hamza Farooq <131591719+HamzahFarooqq@users.noreply.github.com>
2023-06-24 00:40:01 +0500

    loops in php

---
+++ task-27/task27.php
@@ -0,0 +1,31 @@
+<?php 
+
+// Write a Program to display count, from 5 to 15 (as shown below) using following:
+// For Loop
+// While Loop
+// Do…while Loop
+
+
+
+// for loop
+for ($i=5; $i<=15; $i++) {
+    echo $i . '<br>';
+}
+
+
+// while loop
+$i =5;
+while ($i<=15) {
+    
+    echo $i . '<br>';
+    $i++;
+}
+
+// Do...while loop
+$i =5;
+do {
+    echo $i. '<br>';
+    $i++;
+}   while ($i <=15)
+
+?>
