73a47b5b454195accced5f7c2617b6defb2005e9
Hamza Farooq <131591719+HamzahFarooqq@users.noreply.github.com>
2023-06-24 00:41:23 +0500

    output statement in php

---
+++ task-28/task28.php
@@ -0,0 +1,20 @@
+<?php 
+
+// Display your name on the screen using PHP.
+
+echo "<h1> hamza farooq </h1>";
+
+print 'hamza farooq'.'<br>' ;
+
+$x = 'hamza ';
+$y = 'farooq';
+
+echo $x.$y;
+
+
+
+
+
+
+
+?>
