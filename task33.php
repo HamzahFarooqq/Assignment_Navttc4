02e572ee3d01d8bc8614f9a66329cd73ed6007f8
Hamza Farooq <131591719+HamzahFarooqq@users.noreply.github.com>
2023-06-25 11:56:28 +0500

    while loop 1-5 in php

---
+++ task33.php
@@ -0,0 +1,14 @@
+<?php 
+// Write a Program to display count, from 1 to 5 using PHP do...while loop as given below:
+
+
+$a = 1;
+
+do {
+    echo $a . '<br>';
+    $a++;
+
+} while ($a <= 5);
+
+
+?>
