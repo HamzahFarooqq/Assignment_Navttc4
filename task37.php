8bb4e4bcd1cb0a8a9801ef351a08363eaefa0c4e
Hamza Farooq <131591719+HamzahFarooqq@users.noreply.github.com>
2023-07-04 18:35:07 +0500

    find string length in php.

---
+++ task37.php
@@ -0,0 +1,12 @@
+<?php 
+// Write a PHP program to find the length of the string. 
+// Hint:
+// You have to use one variable.
+// You must use a built-in PHP String Function.
+
+$name = 'Maulvi';
+
+echo 'the length of string is : '. strlen($name);
+
+
+?>
